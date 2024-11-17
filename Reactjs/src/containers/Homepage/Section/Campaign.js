b05d1db7cfcf98059eaecb532c5db3bdce3d2b8b
ncquan04 <chiquan02122004@gmail.com>
2024-11-17 23:08:19 +0700

    Fix Slider

--- Reactjs/src/containers/Homepage/Section/Campaign.js
+++ Reactjs/src/containers/Homepage/Section/Campaign.js
@@ -14,7 +14,7 @@ class Campaign extends Component {
             dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
+            slidesToShow: 3,
             slidesToScroll: 1,
         };
         return (
