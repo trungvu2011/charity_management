e39f6c6c7b0bbd13d42e89414a3857f62cca1296
ncquan04 <chiquan02122004@gmail.com>
2024-11-20 16:51:13 +0700

    Add login page

--- Reactjs/src/containers/Auth/Login.js
+++ Reactjs/src/containers/Auth/Login.js
@@ -66,41 +66,33 @@ class Login extends Component {
 
     render() {
         return (
-            <div className='login-background'>
-                <div className='login-container'>
-                    <div className='login-content row'>
-                        <div className='col-12 text-login'>Login</div>
-                        <div className='col-12 form-group login-input'>
-                            <label>Username:</label>
-                            <input type='text' className='form-control' placeholder='Enter your username'
-                                value={this.state.username} onChange={(event) => this.handleOnChangeUsername(event)} />
+            <div className="login-container">
+                <div className='left-container'>
+                    <div className='content-left'></div>
+                </div>
+                <div className='right-container'>
+                    <div className='box-login'>
+                        <div className='login-title'>Đăng nhập</div>
+                        <div className='input-box'>
+                            <label>Email</label>
+                            <input type='text' placeholder='Nhập địa chỉ email' value={this.state.username} onChange={(event) => this.handleOnChangeUsername(event)} />
                         </div>
-                        <div className='col-12 form-group login-input'>
-                            <label>Password:</label>
-                            <div className='custom-input-pastsword'>
-                                <input type={this.state.isShowPassword ? 'text' : 'password'} className='form-control' placeholder='Enter your password'
-                                    value={this.state.password} onChange={(event) => this.handleOnChangePassword(event)} />
-                                <span
-                                    onClick={() => { this.handleShowPassword() }}
-                                ><i className={this.state.isShowPassword ? "far fa-eye" : "far fa-eye-slash"}></i></span>
-                            </div>
+                        <div className='input-box'>
+                            <label>Mật khẩu</label>
+                            <input type={this.state.isShowPassword ? 'text' : 'password'} placeholder='Nhập mật khẩu' value={this.state.password} onChange={(event) => this.handleOnChangePassword(event)} />
+                            <span 
+                                onClick={() => {this.handleShowPassword()}}
+                                >
+                                <i className={this.state.isShowPassword ? "far fa-eye" : "far fa-eye-slash"}/>
+                            </span>
                         </div>
-                        <div className='col-12' style={{ color: 'red' }}>
-                            {this.state.errMessage}
+                        <div className='forgot-password'>
+                            <a href='/forgot-password'>Quên mật khẩu</a>
                         </div>
-                        <div className='col-12'>
-                            <button className='btn-login'
-                                onClick={() => { this.handleLogin() }} >Login</button>
-                        </div>
-                        <div className='col-12'>
-                            <span className='forgot-password'>Forgot your password?</span>
-                        </div>
-                        <div className='col-12 text-center mt-3'>
-                            <span className='text-other-login'>Or Login with:</span>
-                        </div>
-                        <div className='col-12 social-login'>
-                            <i className='fab fa-google-plus-g google'></i>
-                            <i className='fab fa-facebook-f facebook'></i>
+                        <button className='login-button' onClick={this.handleLogin}>Đăng nhập</button>
+                        <div className='register-container'>
+                            <span>Bạn chưa có tài khoản?</span>
+                            <a href='/register'>Đăng ký ngay</a>
                         </div>
                     </div>
                 </div>
